8528deffddceb76d864589dd0772bd637e92c39d
Harshini N <radhaharshini@gmail.com>
2017-08-14 23:23:27 +0530

    Add files via upload

--- Challenges.php
+++ Challenges.php
@@ -37,12 +37,12 @@ and open the template in the editor.
 </nav>
    <div class="container" style="position: relative;">
         <div id="mySidenav" class="sidenav" style="width:25%; margin-top: -20px;">
-            <a href="page2.php" class="btn " disabled style="background-color: #C21E17; height: 80px; padding-top: 15px" >GUIDANCE<br> RECEIVED</a>
-            <a class="btn " disabled href="#" style="background-color: #A11913; height: 80px; padding-top: 15px">FACILITIES <br>AVAILABLE</a>
-            <a class="btn " disabled href="#" style="background-color: #861510 ;height: 80px; padding-top: 15px">WORK AND LIFE<br> BALANCE</a>
-            <a class="btn " disabled href="#" style="background-color: #670F0B; height: 80px; padding-top: 30px;" >PROGRESS</a>
-            <a class="btn active" href="#" style="background-color: #FF8A65; height: 80px; padding-top: 30px;">CHALLENGES</a>
-            <a class="btn " disabled href="#" style="background-color: #4A0707; height: 80px; padding-top: 30px;">COURSE WORK</a>
+            <a href="page2.php" class="btn " disabled style="background-color: #C21E17; height: 60px; padding-top: 15px" >GUIDANCE<br> RECEIVED</a>
+            <a class="btn " disabled href="#" style="background-color: #A11913; height: 60px; padding-top: 15px">FACILITIES <br>AVAILABLE</a>
+            <a class="btn " disabled href="#" style="background-color: #861510 ;height: 60px; padding-top: 15px">WORK AND LIFE<br> BALANCE</a>
+            <a class="btn " disabled href="#" style="background-color: #670F0B; height: 60px; padding-top: 30px;" >PROGRESS</a>
+            <a class="btn active" href="#" style="background-color: #FF8A65; height: 60px; padding-top: 30px;">CHALLENGES</a>
+            <a class="btn " disabled href="#" style="background-color: #4A0707; height: 60px; padding-top: 30px;">COURSE WORK</a>
         </div>
         <div class="scrollable des" style="margin-left: 25%; margin-top: 80px; ;  height :480px;">
         <div style="margin-top : 3%;">
