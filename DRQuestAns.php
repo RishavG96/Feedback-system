58e4b6646100439c92aa7e15d1cd7c0d70e3b283
Rishav Gupta <gupta.rishav2608@gmail.com>
2017-08-23 07:30:51 +0530

    DR QuestAns added

--- DRQuestAns.php
+++ DRQuestAns.php
@@ -21,7 +21,7 @@
             };
         }
     </script>
-        <title>WORK & LIFE</title>
+        <title>Questions</title>
 </head>
 <body style="background-color: #CF4747">
    <nav class="navbar navbar-inverse navbar-fixed-top" >
@@ -106,6 +106,161 @@
                     <tr>
                         <td><b>Have you initiated requests to procure new paid resources</b> </td>
                         <td><?php echo $row1["q8"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>What kind of course are you currently enrolled in</td>
+                        <td><?php echo $row2["q10"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>What do you prefer</td>
+                        <td><?php echo $row2["q11"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>Suggestion Regarding Course Work(12 credits) </td>
+                        <td><?php echo $row2["q12"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>Have you published any conference paper/journal paper in last 6 months </td>
+                        <td><?php echo $row3["q1"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>How do you rate the PhD program overall</td>
+                        <td><?php echo $row3["q2"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>Details of conference/journal paper </td>
+                        <td><?php echo $row3["q3"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>To what extent is your guide available when you had queries/issues</td>
+                        <td><?php echo $row4["q1"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>How useful is your guide's feedback</td>
+                        <td><?php echo $row4["q2"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>To what extent does your guide review the paper/report and presentation on time</td>
+                        <td><?php echo $row4["q3"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>To what extent has your guide encouraged you to interact with external experts</td>
+                        <td><?php echo $row4["q4"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>How often do you receive guidance or feedback from your guide</td>
+                        <td><?php echo $row4["q5"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>Do you feel your guide is biased</td>
+                        <td><?php echo $row4["q6"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>Would you recommend your guide to other candidates</td>
+                        <td><?php echo $row4["q7"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>To what extent do you receive support from the Head of the Department</td>
+                        <td><?php echo $row4["q8"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>Have you completed DAC in the last 6 months</td>
+                        <td><?php echo $row4["q9"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>Did you receive support from your guide during DAC</td>
+                        <td><?php echo $row4["q10"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>Did you receive feedback from DAC members</td>
+                        <td><?php echo $row4["q11"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>How do you rate the work atmosphere in your department</td>
+                        <td><?php echo $row5["q1"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>How many hours do you put in for research work per week</td>
+                        <td><?php echo $row5["q2"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>Are you able to balance your personal life and research work</td>
+                        <td><?php echo $row5["q3"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>Which of the following would you say is the most difficult for PhD's in your discipline</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Learning what career possibilities exist</td>
+                        <td><?php 
+                        if($row5["q4"]=="Learning what career possibilities exist")
+                        {echo "Yes";}
+                        if($row5["q4"]=="NO")
+                        {echo "No";}
+                        ?></td>
+                    </tr>
+                    <tr>
+                        <td>Finding research careers within academia</td>
+                        <td><?php 
+                        if($row5["q5"]=="Finding research careers within academia")
+                        {echo "Yes";}
+                        if($row5["q5"]=="NO")
+                        {echo "No";}
+                        ?></td>
+                    </tr>
+                    <tr>
+                        <td>Finding research careers within industry</td>
+                        <td><?php 
+                        if($row5["q6"]=="Finding research careers within industry")
+                        {echo "Yes";}
+                        if($row5["q6"]=="NO")
+                        {echo "No";}
+                        ?></td>
+                    </tr>
+                    <tr>
+                        <td>Finding research careers within government</td>
+                        <td><?php 
+                        if($row5["q7"]=="Finding research careers within government")
+                        {echo "Yes";}
+                        if($row5["q7"]=="NO")
+                        {echo "No";}
+                        ?></td>
+                    </tr>
+                    <tr>
+                        <td>Finding research careers with charity/non-profit organisations</td>
+                        <td><?php 
+                        if($row5["q8"]=="Finding research careers with charity/non-profit organisations")
+                        {echo "Yes";}
+                        if($row5["q8"]=="NO")
+                        {echo "No";}
+                        ?></td>
+                    </tr>
+                    <tr>
+                        <td>Finding non-research careers that use your skills</td>
+                        <td><?php 
+                        if($row5["q9"]=="Finding non-research careers that use your skills")
+                        {echo "Yes";}
+                        if($row5["q9"]=="NO")
+                        {echo "No";}
+                        ?></td>
+                    </tr>
+                    <tr>
+                        <td>Others</td>
+                        <td><?php 
+                        if($row5["q10"]=="Others")
+                        {echo "Yes";}
+                        if($row5["q10"]=="NO")
+                        {echo "No";}
+                        ?></td>
+                    </tr>
+                    <tr>
+                        <td>What is the scope for your work</td>
+                        <td><?php echo $row6["q1"]; ?></td>
+                    </tr>
+                    <tr>
+                        <td>Any Other Comments</td>
+                        <td><?php echo $row6["q2"]; ?></td>
                     </tr>
                 </tbody>
             </table>
