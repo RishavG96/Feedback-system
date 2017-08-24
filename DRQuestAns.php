4c7d04735aab4e104df345f5ba27814f45e3e0f8
Rishav Gupta <gupta.rishav2608@gmail.com>
2017-08-24 10:33:48 +0530

    Table front end changed

--- DRQuestAns.php
+++ DRQuestAns.php
@@ -73,203 +73,203 @@ if(!isset($_SESSION["drlogin"]))
     $row6 = $result6->fetch_assoc();
     ?>
     <div class="container">
-        <div class="scrollable des" style="margin-top: 90px;">
-            <table class="table table-hover table-striped table-bordered table-responsive" style="color: white;">
+        <div class="scrollable" style="margin-top: 90px;">
+            <table class="table table-hover table-striped table-bordered table-responsive">
                 <tbody>
                     <tr>
-                        <th>Questions</th>
-                        <th>Answers</th>
+                        <th style="background-color: #111111;color: #ffffff"><h4>Questions</h4></th>
+                        <th style="background-color: #111111;color:#ffffff"><h4>Answers</h4></th>
                     </tr>
                     <tr>
-                        <td><b>How do you rate the quality of the following facilities and services available in the campus</b></td>
+                        <td class="outer" ><b>How do you rate the quality of the following facilities and services available in the campus</b></td>
                         <td></td>
                     </tr>
                     <tr>
-                        <td>Leave</td>
-                        <td><?php echo $row1["q1"]; ?></td>
+                        <td class="inner" ><b>Leave</b></td>
+                        <td class="answer"><b><?php echo $row1["q1"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>HealthCare</td>
-                        <td><?php echo $row1["q2"]; ?></td>
+                        <td class="inner" ><b>Health Care</b></td>
+                        <td  class="answer " ><b><?php echo $row1["q2"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>Infrastructure</td>
-                        <td><?php echo $row1["q3"]; ?></td>
+                        <td class="inner" ><b>Infrastructure</b></td>
+                        <td class="answer" ><b><?php echo $row1["q3"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>Resource</td>
-                        <td><?php echo $row1["q4"]; ?></td>
+                        <td class="inner" ><b>Resource</b></td>
+                        <td class="answer"><b><?php echo $row1["q4"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td><b>Have you applied for funds from the Institute/Department in the last 6 months</b></td>
-                        <td><?php echo $row1["q5"]; ?></td>
+                        <td class="outer" ><b>Have you applied for funds from the Institute/Department in the last 6 months</b></td>
+                        <td class="answer" ><b><?php echo $row1["q5"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td><b>Have you initiated research grant submission from an external agency in the last 6 months</b></td>
-                        <td><?php echo $row1["q6"]; ?></td>
+                        <td class="outer"><b>Have you initiated research grant submission from an external agency in the last 6 months</b></td>
+                        <td class="answer"><b><?php echo $row1["q6"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td><b>Have you availed facilities from the University to attend workshop/publish research work</b></td>
-                        <td><?php echo $row1["q7"]; ?></td>
+                        <td class="outer"><b>Have you availed facilities from the University to attend workshop/publish research work</b></td>
+                        <td class="answer" ><b><?php echo $row1["q7"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td><b>Have you initiated requests to procure new paid resources</b> </td>
-                        <td><?php echo $row1["q8"]; ?></td>
+                        <td class="outer" ><b>Have you initiated requests to procure new paid resources</b> </td>
+                        <td class="answer"><b><?php echo $row1["q8"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>What kind of course are you currently enrolled in</td>
-                        <td><?php echo $row2["q10"]; ?></td>
+                        <td class="outer" ><b>What kind of course are you currently enrolled in</b></td>
+                        <td class="answer" ><b><?php echo $row2["q10"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>What do you prefer</td>
-                        <td><?php echo $row2["q11"]; ?></td>
+                        <td class="outer" ><b>What do you prefer</b></td>
+                        <td class="answer" ><b><?php echo $row2["q11"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>Suggestion Regarding Course Work(12 credits) </td>
-                        <td><?php echo $row2["q12"]; ?></td>
+                        <td class="outer"><b>Suggestion Regarding Course Work(12 credits)</b> </td>
+                        <td class="answer" ><b><?php echo $row2["q12"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>Have you published any conference paper/journal paper in last 6 months </td>
-                        <td><?php echo $row3["q1"]; ?></td>
+                        <td class="outer"><b>Have you published any conference paper/journal paper in last 6 months </b></td>
+                        <td class="answer" ><b><?php echo $row3["q1"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>How do you rate the PhD program overall</td>
-                        <td><?php echo $row3["q2"]; ?></td>
+                        <td class="outer" ><b>How do you rate the PhD program overall</b></td>
+                        <td class="answer" ><b><?php echo $row3["q2"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>Details of conference/journal paper </td>
-                        <td><?php echo $row3["q3"]; ?></td>
+                        <td class="outer"><b>Details of conference/journal paper </b></td>
+                        <td class="answer"><b><?php echo $row3["q3"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>To what extent is your guide available when you had queries/issues</td>
-                        <td><?php echo $row4["q1"]; ?></td>
+                        <td  class="outer" ><b>To what extent is your guide available when you had queries/issues</b></td>
+                        <td class="answer" ><b><?php echo $row4["q1"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>How useful is your guide's feedback</td>
-                        <td><?php echo $row4["q2"]; ?></td>
+                        <td class="outer" ><b>How useful is your guide's feedback</b></td>
+                        <td class="answer" ><b><?php echo $row4["q2"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>To what extent does your guide review the paper/report and presentation on time</td>
-                        <td><?php echo $row4["q3"]; ?></td>
+                        <td class="outer" ><b>To what extent does your guide review the paper/report and presentation on time</b></td>
+                        <td class="answer" ><b><?php echo $row4["q3"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>To what extent has your guide encouraged you to interact with external experts</td>
-                        <td><?php echo $row4["q4"]; ?></td>
+                        <td class="outer" ><b>To what extent has your guide encouraged you to interact with external experts</b></td>
+                        <td class="answer" ><b><?php echo $row4["q4"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>How often do you receive guidance or feedback from your guide</td>
-                        <td><?php echo $row4["q5"]; ?></td>
+                        <td class="outer" ><b>How often do you receive guidance or feedback from your guide</b></td>
+                        <td class="answer" ><b><?php echo $row4["q5"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>Do you feel your guide is biased</td>
-                        <td><?php echo $row4["q6"]; ?></td>
+                        <td class="outer" ><b>Do you feel your guide is biased</b></td>
+                        <td class="answer" ><b><?php echo $row4["q6"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>Would you recommend your guide to other candidates</td>
-                        <td><?php echo $row4["q7"]; ?></td>
+                        <td class="outer" ><b>Would you recommend your guide to other candidates</b></td>
+                        <td class="answer" ><b><?php echo $row4["q7"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>To what extent do you receive support from the Head of the Department</td>
-                        <td><?php echo $row4["q8"]; ?></td>
+                        <td class="outer" ><b>To what extent do you receive support from the Head of the Department</b></td>
+                        <td class="answer"><b><?php echo $row4["q8"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>Have you completed DAC in the last 6 months</td>
-                        <td><?php echo $row4["q9"]; ?></td>
+                        <td class="outer"><b>Have you completed DAC in the last 6 months</b></td>
+                        <td class="answer"><b><?php echo $row4["q9"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>Did you receive support from your guide during DAC</td>
-                        <td><?php echo $row4["q10"]; ?></td>
+                        <td class="outer"><b>Did you receive support from your guide during DAC</b></td>
+                        <td class="answer" ><b><?php echo $row4["q10"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>Did you receive feedback from DAC members</td>
-                        <td><?php echo $row4["q11"]; ?></td>
+                        <td class="outer" ><b>Did you receive feedback from DAC members</b></td>
+                        <td class="answer"><b><?php echo $row4["q11"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>How do you rate the work atmosphere in your department</td>
-                        <td><?php echo $row5["q1"]; ?></td>
+                        <td class="outer" ><b>How do you rate the work atmosphere in your department</b></td>
+                        <td class="answer" ><b><?php echo $row5["q1"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>How many hours do you put in for research work per week</td>
-                        <td><?php echo $row5["q2"]; ?></td>
+                        <td class="outer" ><b>How many hours do you put in for research work per week</b></td>
+                        <td class="answer" ><b><?php echo $row5["q2"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>Are you able to balance your personal life and research work</td>
-                        <td><?php echo $row5["q3"]; ?></td>
+                        <td class="outer" ><b>Are you able to balance your personal life and research work</b></td>
+                        <td class="answer"><b><?php echo $row5["q3"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>Which of the following would you say is the most difficult for PhD's in your discipline</td>
+                        <td class="outer"><b>Which of the following would you say is the most difficult for PhD's in your discipline</b></td>
                         <td></td>
                     </tr>
                     <tr>
-                        <td>Learning what career possibilities exist</td>
-                        <td><?php 
+                        <td class="inner" ><b>Learning what career possibilities exist</b></td>
+                        <td class="answer" ><b><?php 
                         if($row5["q4"]=="Learning what career possibilities exist")
                         {echo "Yes";}
                         if($row5["q4"]=="NO")
                         {echo "No";}
-                        ?></td>
+                        ?></b></td>
                     </tr>
                     <tr>
-                        <td>Finding research careers within academia</td>
-                        <td><?php 
+                        <td class="inner"><b>Finding research careers within academia</b></td>
+                        <td class="answer"><b><?php 
                         if($row5["q5"]=="Finding research careers within academia")
                         {echo "Yes";}
                         if($row5["q5"]=="NO")
                         {echo "No";}
-                        ?></td>
+                        ?></b></td>
                     </tr>
                     <tr>
-                        <td>Finding research careers within industry</td>
-                        <td><?php 
+                        <td class="inner" ><b>Finding research careers within industry</b></td>
+                        <td class="answer" ><b><?php 
                         if($row5["q6"]=="Finding research careers within industry")
                         {echo "Yes";}
                         if($row5["q6"]=="NO")
                         {echo "No";}
-                        ?></td>
+                        ?></b></td>
                     </tr>
                     <tr>
-                        <td>Finding research careers within government</td>
-                        <td><?php 
+                        <td class="inner" ><b>Finding research careers within government</b></td>
+                        <td class="answer" ><b><?php 
                         if($row5["q7"]=="Finding research careers within government")
                         {echo "Yes";}
                         if($row5["q7"]=="NO")
                         {echo "No";}
-                        ?></td>
+                        ?></b></td>
                     </tr>
                     <tr>
-                        <td>Finding research careers with charity/non-profit organisations</td>
-                        <td><?php 
+                        <td class="inner" ><b>Finding research careers with charity/non-profit organisations</b></td>
+                        <td class="answer"><b><?php 
                         if($row5["q8"]=="Finding research careers with charity/non-profit organisations")
                         {echo "Yes";}
                         if($row5["q8"]=="NO")
                         {echo "No";}
-                        ?></td>
+                        ?></b></td>
                     </tr>
                     <tr>
-                        <td>Finding non-research careers that use your skills</td>
-                        <td><?php 
+                        <td class="inner"><b>Finding non-research careers that use your skills</b></td>
+                        <td  class="answer" ><b><?php 
                         if($row5["q9"]=="Finding non-research careers that use your skills")
                         {echo "Yes";}
                         if($row5["q9"]=="NO")
                         {echo "No";}
-                        ?></td>
+                        ?></b></td>
                     </tr>
                     <tr>
-                        <td>Others</td>
-                        <td><?php 
+                        <td class="inner"><b>Others</b></td>
+                        <td class="answer" ><b><?php 
                         if($row5["q10"]=="Others")
                         {echo "Yes";}
                         if($row5["q10"]=="NO")
                         {echo "No";}
-                        ?></td>
+                        ?></b></td>
                     </tr>
                     <tr>
-                        <td>What is the scope for your work</td>
-                        <td><?php echo $row6["q1"]; ?></td>
+                        <td class="outer" ><b>What is the scope for your work</b></td>
+                        <td class="answer"><b><?php echo $row6["q1"]; ?></b></td>
                     </tr>
                     <tr>
-                        <td>Any Other Comments</td>
-                        <td><?php echo $row6["q2"]; ?></td>
+                        <td class="outer" ><b>Any Other Comments</b></td>
+                        <td class="answer" ><b><?php echo $row6["q2"]; ?></b></td>
                     </tr>
                 </tbody>
             </table>
